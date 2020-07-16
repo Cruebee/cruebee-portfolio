b333aa87dca3fca9c7913855bcbc11c1cddbf94d
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-07-16 16:03:41 -0600

    update styling for about Me cards, Create initial card component for various projects, prepare to use MODALS to display project details

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 export default class ProjectCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   rener() {
     return (
       <Card>
         <Card.Body>
-          <Card.Title>{this.project.title}</Card.Title>
+          <Card.Title>ProjectTitle</Card.Title>
         </Card.Body>
       </Card>
     );
